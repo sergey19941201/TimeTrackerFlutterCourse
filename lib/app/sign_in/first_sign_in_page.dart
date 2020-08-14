dde472a679cf5c7324873c0b9cbf18637445b0fc
Sergey Koshelnyk <serhii.koshelnyk@extendaretail.com>
2020-08-14 21:25:07 +0300

    show circular progress indicator and block buttons when _isLoading = true

--- lib/app/sign_in/first_sign_in_page.dart
+++ lib/app/sign_in/first_sign_in_page.dart
@@ -7,7 +7,14 @@ import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.da
 import 'package:time_tracker_flutter_course/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
-class FirstSignInPage extends StatelessWidget {
+class FirstSignInPage extends StatefulWidget {
+  @override
+  _FirstSignInPageState createState() => _FirstSignInPageState();
+}
+
+class _FirstSignInPageState extends State<FirstSignInPage> {
+  bool _isLoading = false;
+
   void _showSignInError(BuildContext context, PlatformException exception) {
     PlatformExceptionAlertDialog(
       title: 'Sign in failed',
@@ -26,11 +33,15 @@ class FirstSignInPage extends StatelessWidget {
 
   Future<void> _signInAnonymously(BuildContext context) async {
     try {
+      setState(() => _isLoading = true);
+      await Future.delayed(const Duration(seconds: 1));
       final auth = Provider.of<AuthBase>(context, listen: false);
       User user = await auth.signInAnonymously();
       //onSignIn(user);
     } on PlatformException catch (e) {
       _showSignInError(context, e);
+    } finally {
+      setState(() => _isLoading = false);
     }
   }
 
@@ -53,18 +64,14 @@ class FirstSignInPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text(
-            'Sign in',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
-          ),
+          SizedBox(height: 50, child: _buildHeader()),
           SizedBox(height: 48),
           SocialSignInButton(
             assetName: 'images/google-logo.png',
             text: 'Sign in with Google',
             textColor: Colors.black87,
             color: Colors.white,
-            onPressed: () {},
+            onPressed: _isLoading ? null : () {},
           ),
           SizedBox(height: 8.0),
           SocialSignInButton(
@@ -72,14 +79,14 @@ class FirstSignInPage extends StatelessWidget {
             text: 'Sign in with Facebook',
             textColor: Colors.white,
             color: Color(0xFF334D92),
-            onPressed: () {},
+            onPressed: () => _isLoading ? null : () {},
           ),
           SizedBox(height: 8.0),
           SignInButton(
             text: 'Sign in with email',
             textColor: Colors.white,
             color: Colors.teal[700],
-            onPressed: () => _signInWithEmail(context),
+            onPressed: _isLoading ? null : () => _signInWithEmail(context),
           ),
           SizedBox(height: 8.0),
           Text(
@@ -92,9 +99,25 @@ class FirstSignInPage extends StatelessWidget {
             text: 'Go anonymous',
             textColor: Colors.black,
             color: Colors.lime[300],
-            onPressed: () => _signInAnonymously(context),
+            onPressed: _isLoading ? null : () => _signInAnonymously(context),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    if (_isLoading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Text(
+      'Sign in',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w600,
       ),
     );
   }
