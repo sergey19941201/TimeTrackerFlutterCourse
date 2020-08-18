64b952dfb58c12702990e314b0005ff746f7eb7e
Sergey Koshelnyk <serhii.koshelnyk@extendaretail.com>
2020-08-18 17:12:29 +0300

    add job

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/add_job_page.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/job_list_tile.dart';
 import 'package:time_tracker_flutter_course/app/home/models/job.dart';
 import 'package:time_tracker_flutter_course/common_widgets/platform_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
@@ -24,11 +26,6 @@ class JobsPage extends StatelessWidget {
     }
   }
 
-  void _createJob(BuildContext context) {
-    final database = Provider.of<Database>(context, listen: false);
-    database.createJob(Job(name: 'Blogging', ratePerHour: 10));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +42,7 @@ class JobsPage extends StatelessWidget {
       body: _buildContents(context),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => _createJob(context),
+        onPressed: () => AddJobPage.show(context), //_createJob(context),
       ),
     );
   }
@@ -57,10 +54,15 @@ class JobsPage extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final jobs = snapshot.data;
-          final children = jobs.map((job) => Text(job.name)).toList();
+          final children = jobs
+              .map((job) => JobListTile(
+                    job: job,
+                    onTap: () {},
+                  ))
+              .toList();
           return ListView(children: children);
         }
-        if(snapshot.hasError){
+        if (snapshot.hasError) {
           return Center(child: Text('Some error occured'));
         }
         return Center(child: CircularProgressIndicator());
